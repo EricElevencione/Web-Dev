a8fec297b8c4becf86aa7764f0958b36e0db90c5
Eric Elevencione <silvafang0@gmail.com>
2024-09-02 10:03:44 +0800

    Update

--- index.php
+++ index.php
@@ -3,10 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Web-Dev//assets//style.css">
+    <link rel="stylesheet" href="..//Web-Dev/assets//style.css">
     <title>Document</title>
 </head>
 <body>
-    Hello World
+    <div id="hero">
+        <div class="nav">
+            <img src="..//Web-Dev//assets//images//pngwing.com.png" alt="" width="250" height="150">
+            <button class="btn">Home</button>
+            <button class="btn">Shop</button>
+            <button class="btn">About Us</button>
+            <button class="btn">Contacts</button>
+        </div>
+        <div class="background-img">
+
+        </div>
+    </div>
+    <div id="body"></div>
+    <div id="footer"></div>
 </body>
 </html>
